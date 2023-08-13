6a79b789f3c5c5dcd8a31b83f920890b00c338ec
Steve Winter <stevewinter@outlook.de>
2023-08-13 07:31:37 +0200

    fix(vim): install vim plug for vim and nvim

--- tools/vim.sh
+++ tools/vim.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# install vim-plug
+# install vim-plug for vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# install vim-plug for nvim
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # install vim plugins
