25d497785204b3dbed3e40d111400f3afaa9f596
Steve Winter <stevewinter@outlook.de>
2023-08-13 07:46:14 +0200

    fix(brew): update brew installation

--- tools/homebrew.sh
+++ tools/homebrew.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
+# install brew
 echo -e "\033[1m\033[34m==> Installing brew\033[0m"
-if [[ $(which brew) == "/usr/local/bin/brew" ]]
-then
-	    echo "Brew installed already, skipping"
-    else
-	        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 echo -e "\033[1m\033[34m==> Installing brew formulas\033[0m"
 brew bundle --file=~/.config/Brewfile
